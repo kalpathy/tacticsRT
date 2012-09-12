2a1f83865a34be45531e6eb880554f25bd66bdad
Jayashree Kalpathy-Cramer <kalpathy@nmr.mgh.harvard.edu>
2012-09-12 16:29:41 -0400

    removed unnecessary files

--- generate/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '479dc95129b5751edc7dc67458ce598bd9d0ae67e6df08031013bbce8990897070e23fedbf370abeea9b25ad17a31c9d5803d8dd09604a936db27c1eb0b390c7';
